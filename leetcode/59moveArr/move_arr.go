db3b1ba22ab261330460b4427fda3a8e7d18d8c9
dengguijiang <dgjungle_c@163.com>
2020-09-28 18:05:22 +0800

    clean project

--- leetcode/59moveArr/move_arr.go
+++ leetcode/59moveArr/move_arr.go
@@ -1,7 +1,27 @@
 package main
 
 func maxSlidingWindow(nums []int, k int) []int {
-
+	result := make([]int, 0, 0)
+	if len(nums) == 0 || k == 0 {
+		return result
+	}
+	for i := 0; i < len(nums)-k+1; i++ {
+		result = append(result, maxNum(nums[i:i+k]))
+	}
+	return result
+}
+func maxNum(nums []int) int {
+	max := nums[0]
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
 }
 
-func main() {}
+func main() {
+	nums := []int{1, -1}
+	maxSlidingWindow(nums, 1)
+
+}
