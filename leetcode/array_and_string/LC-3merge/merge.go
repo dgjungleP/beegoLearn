1e3795f8dfcc7682acf3c0b8e2e6625724f45e22
dengguijiang <dgjungle_c@163.com>
2020-11-03 17:56:21 +0800

    数组

--- leetcode/array_and_string/LC-3merge/merge.go
+++ leetcode/array_and_string/LC-3merge/merge.go
@@ -1,11 +1,41 @@
 package main
 
-func merge(intervals [][]int) [][]int {
-	result := make([][]int, 0)
+import "fmt"
 
+func merge(intervals [][]int) [][]int {
+	if len(intervals) < 2 {
+		return intervals
+	}
+	intervals = sort(intervals)
+	result := make([][]int, 0)
+	result = append(result, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		if result[len(result)-1][1] >= intervals[i][0] {
+			if result[len(result)-1][1] < intervals[i][1] {
+				result[len(result)-1][1] = intervals[i][1]
+			}
+			if result[len(result)-1][0] >= intervals[i][0] {
+				result[len(result)-1][0] = intervals[i][0]
+			}
+		} else {
+			result = append(result, intervals[i])
+		}
+	}
 	return result
+}
+func sort(intervals [][]int) [][]int {
+	for i := 0; i < len(intervals); i++ {
+		for j := i; j < len(intervals); j++ {
+			if intervals[j][0] <= intervals[i][0] {
+				temp := intervals[i]
+				intervals[i] = intervals[j]
+				intervals[j] = temp
+			}
+		}
+	}
+	return intervals
 }
 
 func main() {
-
+	fmt.Println(merge([][]int{{2, 3}, {4, 5}, {6, 7}, {8, 9}, {1, 10}}))
 }
