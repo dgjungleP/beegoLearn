3904350dd7cb85c9e0cde4f5105d09df0f5be81c
dengguijiang <dgjungle_c@163.com>
2020-12-30 09:24:30 +0800

    2020-12-30 day prictice

---
+++ leetcode/libraries/simple/LC-1046lastStoneWeight/lastStoneWeight.go
@@ -0,0 +1,20 @@
+package main
+
+import "sort"
+
+func lastStoneWeight(stones []int) int {
+	if len(stones) == 1 {
+		return stones[0]
+	}
+	size := len(stones)
+	sort.Ints(stones)
+	for stones[size-2] != 0 {
+		stones[size-1] -= stones[size-2]
+		stones[size-2] = 0
+		sort.Ints(stones)
+	}
+	return stones[size-1]
+}
+func main() {
+
+}
